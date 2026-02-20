27dce2afae8f031bb5857498ea32b7f2d1a91428
ragpark <pcoyne@bcs.org>
2026-02-20 10:28:56 +0000

    Update Renewal Declined check to allow 'Yes' and 'No'
    Records where renewal decline allowed for Yes and No in subs report

--- app.py
+++ app.py
@@ -89,8 +89,9 @@ def row_invalid(row):
     Returns True if any required field is missing or invalid.
     Checks: School key, School name, Email, Postcode, AH ISBN 1, AH SKU 1, AH Package 1
     """
-    # Check Renewal Declined
-    if pd.notna(row.get("Renewal Declined")) and str(row["Renewal Declined"]).strip().lower() == "yes":
+    # Check Renewal Declined - disabled and allows for Yes and No for Wave 2
+    #if pd.notna(row.get("Renewal Declined")) and str(row["Renewal Declined"]).strip().lower() == "yes":
+    if pd.notna(row.get("Renewal Declined")) and str(row["Renewal Declined"]).strip().lower() in {"yes", "no"}:
         return True
 
     # Check required fields
